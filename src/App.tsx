5b268fb2573d0f9554d29a64e2ff8042223703b3
rishabh-341 <2019129@iiitdmj.ac.in>
2023-12-25 02:26:14 +0530

    Updated imports and dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+
 import AddToDo from "./components/addToDo";
 import "./App.css";
 import TodoList from "./components/todoList";
